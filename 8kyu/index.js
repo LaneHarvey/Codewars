2150e7835e171549a2e4b5357a4c3355e874335d
Lane Harvey <mrlaneharvey@gmail.com>
2017-02-12 17:37:57 -0700

    Sum of positive

--- 8kyu/index.js
+++ 8kyu/index.js
@@ -1,3 +1,4 @@
+// Sum of positive
 // You get an array of numbers, return the sum of all of the positives ones.
 
 function positiveSum(arr) {
@@ -10,10 +11,8 @@ function positiveSum(arr) {
   return sum;
 }
 
-positiveSum([1,3,5,6,-8,2])
-
-// Best Practice
-
 function positiveSum(arr) {
    return arr.reduce((a,b)=> a + (b > 0 ? b : 0),0);
 }
+
+// Sum of positive Best Practice uses reduce
