2c55265b4cfefc13cf53e89eb665f9d35e75656d
Lane Harvey <mrlaneharvey@gmail.com>
2017-02-12 17:45:43 -0700

    Volume of a Cuboid

--- 8kyu/index.js
+++ 8kyu/index.js
@@ -16,3 +16,21 @@ function positiveSum(arr) {
 }
 
 // Sum of positive Best Practice uses reduce
+
+// Volume of a Cuboid
+// Bob needs a fast way to calculate the volume of a cuboid with three values: length, width and the height of the cuboid.
+// Write a function to help Bob with this calculation.
+var Kata;
+
+Kata = (function() {
+  function Kata() {}
+
+  Kata.getVolumeOfCuboid = function(length, width, height) {
+    return length * width * height;
+  };
+
+  return Kata;
+
+})();
+
+Kata.getVolumeOfCuboid(1,2,3);
