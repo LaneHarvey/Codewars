2d5ad8d5503b3c7bd037beaa665b7483fa5e9473
Lane Harvey <mrlaneharvey@gmail.com>
2017-02-13 17:11:15 -0700

    Fake Binary

--- 8kyu/index.js
+++ 8kyu/index.js
@@ -2,7 +2,7 @@
 // Given a string of digits, you should replace any digit below 5 with '0' and any digit 5 and above with '1'.
 // Return the resulting string.
 
-function fakeBin(x){
+function fakeBin(x) {
   var split = x.split('');
   var result = '';
   for (var i=0; i<split.length; i++){
