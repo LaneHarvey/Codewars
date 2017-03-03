c2320ec8caa9444d9112c1a0748262b0d4567816
Lane Harvey <mrlaneharvey@gmail.com>
2017-03-02 22:40:10 -0700

    time correct

--- 7kyu/index.js
+++ 7kyu/index.js
@@ -13,8 +13,12 @@ arrowArea(7,6);
 
 // Correct the time-string
 
-// You have to create a method, that corrects a given time string. There was a problem in addition, so many of the time strings are broken. Time-Format is european. So from "00:00:00" to "23:59:59".
-// If the input-string is null or empty return exactly this value! (empty string for C++)
+// You have to create a method, that corrects a given time string.
+
+// There was a problem in addition, so many of the time strings are broken. Time-Format is european. So from "00:00:00" to "23:59:59".
+
+// If the input-string is null or empty return exactly this value!
+
 // If the time-string-format is invalid, return null.
 
 function timeCorrect(timestring) {
