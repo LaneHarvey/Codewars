8fc859ff793695e8fdbc3755d3c709d614b01799
Lane Harvey <mrlaneharvey@gmail.com>
2017-03-02 22:39:04 -0700

    time correct

--- 7kyu/index.js
+++ 7kyu/index.js
@@ -11,6 +11,17 @@ function arrowArea(a,b) {
 }
 arrowArea(7,6);
 
+// Correct the time-string
+
+// You have to create a method, that corrects a given time string. There was a problem in addition, so many of the time strings are broken. Time-Format is european. So from "00:00:00" to "23:59:59".
+// If the input-string is null or empty return exactly this value! (empty string for C++)
+// If the time-string-format is invalid, return null.
+
+function timeCorrect(timestring) {
+
+}
+
+
 // Mumbling
 
 // This time no story, no theory. The examples below show you how to write function accum:
